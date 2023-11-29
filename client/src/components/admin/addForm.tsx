050626c6d72929def5c0ff96f04156192aa54c57
Alexis1226 <feuri41@gmail.com>
2023-11-29 19:16:45 +0900

    admin addform 수정

--- client/src/components/admin/addForm.tsx
+++ client/src/components/admin/addForm.tsx
@@ -1,11 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import { QueryKeys, getClient, graphQlFetcher } from '../../queryClient';
 import { ADD_PRODUCT, MutableProduct } from '../../graphql/products';
 import arrToObj from '../../utill/arrToObj';
 
 const AddForm = () => {
   const queryClient = getClient();
+  const [submitDisable, setSubmitDisable] = useState(false);
   const { mutate: addProduct } = useMutation(
     ({ title, imageUrl, price, description }: MutableProduct) =>
       graphQlFetcher(ADD_PRODUCT, { title, imageUrl, price, description }),
@@ -16,6 +17,7 @@ const AddForm = () => {
           exact: false,
           refetchType: 'all',
         });
+        setSubmitDisable(false);
       },
     }
   );
@@ -24,27 +26,28 @@ const AddForm = () => {
     e.preventDefault();
     const formData = arrToObj([...new FormData(e.target as HTMLFormElement)]);
     formData.price = Number(formData.price);
+    setSubmitDisable(true);
     addProduct(formData as MutableProduct);
+    (e?.target as HTMLFormElement).reset();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="form__adminAdd" onSubmit={handleSubmit}>
       <label htmlFor="title">
-        상품명 : <input name="title" type="text" />
+        상품명 :<input name="title" id="title" type="text" required />
       </label>
       <label htmlFor="imageUrl">
-        이미지URL:
-        <input name="imageUrl" type="text" />
+        이미지URL :<input name="imageUrl" id="imageUrl" type="text" required />
       </label>
       <label htmlFor="price">
-        상품가격:
-        <input name="price" type="text" required min="1000" />
+        상품가격 :<input name="price" id="price" type="text" required min="1000" />
       </label>
       <label htmlFor="description">
-        상세 :
-        <textarea name="description" />
+        상세 :<textarea name="description" id="description" required />
       </label>
-      <button type="submit">등록</button>
+      <button type="submit" disabled={submitDisable}>
+        등록
+      </button>
     </form>
   );
 };
